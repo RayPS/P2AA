b36dbb5354012788aaff6dec03de432d6ea7ec19
Ray <ray@rayps.com>
2015-04-10 01:04:48 +0800

    First commit

---
+++ P2AA/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  P2AA
+//
+//  Created by Ray on 4/10/15.
+//  Copyright (c) 2015 RayPS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
